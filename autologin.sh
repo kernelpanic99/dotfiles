fb1f92b790137e9c6480db63f72ddfec6786b727
Arthur Nikolaienko <n>
2026-01-26 23:35:12 +0200

    autologin setup script

---
+++ autologin.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin kp --noclear %I \$TERM
+EOF
