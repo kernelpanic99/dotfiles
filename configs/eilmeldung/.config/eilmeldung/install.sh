eca13b83fc86cfad489ce1b03dd9145ad14adb05
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-06-23 22:35:45 +0300

    Cleanup legacy configs, get rid of git crypt and submodules

--- configs/eilmeldung/.config/eilmeldung/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp ~/.config/eilmeldung/{eilmeldung-sync.timer,eilmeldung-sync.service} ~/.config/systemd/user/
-
-systemctl --user daemon-reload
-systemctl --user enable --now eilmeldung-sync.timer
