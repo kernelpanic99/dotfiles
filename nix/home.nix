e781419b3206c2d0ff235955bdb397a975bb1e16
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-08-30 21:55:11 +0300

    nix: direnv silent mode

--- nix/home.nix
+++ nix/home.nix
@@ -219,6 +219,13 @@
     direnv = {
       enable = true;
       nix-direnv.enable = true;
+      enableFishIntegration = true;
+
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
+      };
     };
   };
 
