8ef86b69c19702a4706767b7a98284dee5d4ea49
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-09-20 16:35:23 +0300

    noctalia: bring settings into the repo

--- nix/config/noctalia/noctalia.nix
+++ nix/config/noctalia/noctalia.nix
@@ -64,56 +64,6 @@
 
     location.address = "Odessa, Ukraine";
 
-    lockscreen_widgets = {
-      enabled = false;
-      schema_version = 2;
-      widget_order = ["lockscreen-login-box@eDP-1" "lockscreen-login-box@HDMI-A-1"];
-
-      grid = {
-        cell_size = 16;
-        major_interval = 4;
-        visible = true;
-      };
-
-      widget = {
-        "lockscreen-login-box@HDMI-A-1" = {
-          box_height = 70.0;
-          box_width = 400.0;
-          cx = 960.0;
-          cy = 961.0;
-          output = "HDMI-A-1";
-          rotation = 0.0;
-          type = "login_box";
-          settings = {
-            background_color = "surface_variant";
-            background_opacity = 0.88;
-            background_radius = 12.0;
-            input_opacity = 1.0;
-            input_radius = 6.0;
-            show_login_button = true;
-          };
-        };
-
-        "lockscreen-login-box@eDP-1" = {
-          box_height = 70.0;
-          box_width = 400.0;
-          cx = 768.0;
-          cy = 745.0;
-          output = "eDP-1";
-          rotation = 0.0;
-          type = "login_box";
-          settings = {
-            background_color = "surface_variant";
-            background_opacity = 0.88;
-            background_radius = 12.0;
-            input_opacity = 1.0;
-            input_radius = 6.0;
-            show_login_button = true;
-          };
-        };
-      };
-    };
-
     shell = {
       font_family = "ComicShannsMono Nerd Font Mono";
       screenshot = {
@@ -132,11 +82,10 @@
     };
 
     wallpaper = {
-      default.path = "/home/kp/Pictures/Wallpapers/flowers-19.jpg";
-      last.path = "/home/kp/Pictures/Wallpapers/flowers-19.jpg";
-      monitors = {
-        "HDMI-A-1".path = "/home/kp/Pictures/Wallpapers/flowers-19.jpg";
-        "eDP-1".path = "/home/kp/Pictures/Wallpapers/flowers-19.jpg";
+      directory = "~/Pictures/Wallpapers";
+      automation = {
+        enabled = true;
+        interval_seconds = 600;
       };
     };
 
